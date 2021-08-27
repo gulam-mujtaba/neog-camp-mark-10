1ff43df6cb1d21b720ba47f7e3c3a5a565d4e031
Gulam Mujtaba <gulammujtaba35@yahoo.com>
2021-08-27 22:05:34 +0530

    Added Mark-10 Project

---
+++ index.js
@@ -0,0 +1,42 @@
+const billamt = document.querySelector("#bill-amount");
+const cashgvn = document.querySelector("#cash-given");
+const changebtn = document.querySelector("#click");
+const errmsg = document.querySelector("#error-messege");
+const noofnotes = document.querySelectorAll(".notes");
+
+const avlnotes = [2000,500,200,100,20,10,5,1];
+
+changebtn.addEventListener("click", function clickbutton(){
+    messege.style.display = "none";
+
+    if (billamt.value > 0){
+        if(cashgvn.value >= billamt.value){
+            const returnamt = cashgvn.value - billamt.value;
+            changecalculator(returnamt);
+        }else{
+            messegegiven("Cash given is less than bill amount. Please pay the bill amount , warna kaam par lag jao");
+        }
+
+        }else{
+            messegegiven("Invalid Amount Entered!!");
+
+        }
+
+    }
+)};
+
+function messegegiven (text){
+    messegegiven.style.display = "block";
+    messege.innerText = text;
+}
+ function changecalculator (returnamt){
+     for(i=0; i > avlnotes.length; i++){
+         const numberofnotes = Math.trunc(returnamt/avlnotes[i] );
+         returnamt %= avlnotes[i];
+         noofnotes[i].innerText = noofnotes;
+     }
+
+ }
+
+
+
